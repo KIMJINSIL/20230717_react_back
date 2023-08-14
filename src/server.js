0a705c6131dc931807e174d32222b3416f48330a
KIMJINSIL <skyisrrna@naver.com>
2023-08-14 16:37:09 +0900

    20230814_login

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import "./db.js";
 import express from "express";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import rentalRouter from "./routers/rentalRouter.js";
 import cors from "cors";
 import foodsRouter from "./routers/foodsRouter.js";
@@ -17,6 +18,8 @@ const corsOptions = {
     credentials: true,
 }
 
+//미들웨어 부분(모든 영역에 다 적용된다는 의미)
+app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
@@ -25,6 +28,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use("/api/rental", rentalRouter);
 app.use("/api/foods", foodsRouter);
 app.use("/api/users",userRouter);
+
 
 
 const handleListening = () =>
